5b502ac5bdd7c6ab67977592e30307f09470e230
iassafi <iassafi@LFR015837.corp.capgemini.com>
2016-04-27 15:43:40 +0100

    Saisie et récupération du mot clé (html+servlet)

---
+++ src/servlet/SaisieMotClé.java
@@ -0,0 +1,47 @@
+package servlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SaisieMotClé
+ */
+@WebServlet("/SaisieMotClé")
+public class SaisieMotClé extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SaisieMotClé() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void processRequest (HttpServletRequest request, HttpServletResponse response) throws IOException{
+    	response.setContentType("text/html");
+    	PrintWriter out = response.getWriter();
+    	String mot = request.getParameter("keyword");
+    	out.println("vous avez cherché <b>"+mot+ "</b>");
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+}
