85a9b2922a7f2257dfcd0404f69522834c20becf
mopsoner <mopsoner@gmail.com>
2016-05-03 16:37:48 +0100

    doc to json

--- src/model/CvParser.java
+++ src/model/CvParser.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ContentHandler;
@@ -11,12 +12,61 @@ import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.sax.ToXMLContentHandler;
 import org.xml.sax.SAXException;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CvParser {
 
+	int id;
+	String auteur;
+	String date;
+	String texte;
+	
 	public CvParser() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public CvParser(int id,String auteur,String date,String texte) {
+		this.id=id;
+		this.auteur=auteur;
+		this.date=date;
+		this.texte=texte;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTexte() {
+		return texte;
+	}
+
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
+
 	public String parseToString(String uri) throws IOException, SAXException, TikaException {
 	    Tika tika = new Tika();
 	    try (InputStream stream = CvParser.class.getResourceAsStream(uri)) {
@@ -33,6 +83,27 @@ public class CvParser {
 	        return metadata;
 	    }
 	}
+	
+
+	
+
+	
+	String toJSON(String uri) throws JsonProcessingException{
+		ObjectMapper mapper = new ObjectMapper();
+		
+		//Object to JSON in file
+	/*	try {
+			mapper.writeValue(new File(uri), this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+*/
+		//Object to JSON in String
+		return  mapper.writeValueAsString(this);
+		
+		
+	}
 
 	public static void main(String[] args)  {
 		
@@ -44,10 +115,14 @@ public class CvParser {
 			System.out.println(metadata.get("Last-Author"));
 			System.out.println(metadata.get("Last-Save-Date"));
 			System.out.println(text);
+			////
+			CvParser cv2=new CvParser(0, metadata.get("Last-Author"),metadata.get("Last-Save-Date"), text);
+			cv2.toJSON("cv.json");
 		} catch (IOException | SAXException | TikaException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 		
 		
